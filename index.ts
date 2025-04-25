4b6bb61ca21da3910a0a0f5c99ee562aff31c98d
DXCore Technology <i.love.to.smile.around@gmail.com>
2025-04-25 15:16:31 +0700

    upd

--- index.ts
+++ index.ts
@@ -3,5 +3,12 @@
 // Import the default export from the mini-app template
 import MiniApp from '@/templates/mini-app';
 
-// Re-export the component
-export default MiniApp; 
+// Import the WebApp component for standalone use
+import WebApp from './web-app';
+
+// Export all hooks
+export * from './hooks';
+
+// Re-export the components
+export default MiniApp;
+export { WebApp }; 
